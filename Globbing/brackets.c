b3baa12b2e0fd0147cdbc124b5f74f6008bcff0a
Zenande GODONGWANA <zgodongw@e4r14p5.wethinkcode.co.za>
2017-09-22 12:06:27 +0200

    brackets finally

---
+++ Globbing/brackets.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brackets.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zgodongw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/22 12:03:22 by zgodongw          #+#    #+#             */
+/*   Updated: 2017/09/22 12:03:33 by zgodongw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int		braclose(char *str, char c, int i, int b)
+{
+	while (b && *(++str) && (i++))
+	{
+		if (*str == c || *str == c + c % 2 + 1)
+		{
+			if (*str == c)
+				++b;
+			else
+				--b;
+		}
+	}
+	return (i);
+}
+
+int				brackets(char *str, char c)
+{
+	if (*str == c)
+		return (1);
+	else if (!*str || *str == ')' || *str == '}' || *str == ']')
+		return (0);
+	else if (*str == '(' || *str == '{' || *str == '[')
+		return (brackets(str + 1, *str + *str % 2 + 1) *
+				brackets(str + braclose(str, *str, 1, 1), c));
+	else
+		return (brackets(str + 1, c));
+}
