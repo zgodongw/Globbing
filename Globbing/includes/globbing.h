73538da02b939f9fe2b271c19406fff9ec731d26
Zenande GODONGWANA <zgodongw@e4r14p5.wethinkcode.co.za>
2017-09-23 16:59:56 +0200

    New functions

--- Globbing/includes/globbing.h
+++ Globbing/includes/globbing.h
@@ -6,7 +6,7 @@
 /*   By: zgodongw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/22 11:55:45 by zgodongw          #+#    #+#             */
-/*   Updated: 2017/09/22 12:14:33 by zgodongw         ###   ########.fr       */
+/*   Updated: 2017/09/23 16:55:00 by zgodongw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,16 @@
 int		ft_strncmp(const char*, const char*, size_t);
 int		ft_strlen(const char*);
 char	*ft_strstr(const char*, const char*);
+char	*ft_strchr(const char *s, int c);
 int		isglob(const char*);
+int		rangeisvalid(const char*);
+int		ft_isalnumextended(char);
 int		starglob(const char*, char*);
 int		quesglob(const char*, char*);
 int		bracglob(const char*, char*);
 int		globfunction(const char*, char*);
 int		brackets(const char*, char);
+char	**listtoarray(t_list*);
+t_list	*globinit(char *);
 
 # endif
